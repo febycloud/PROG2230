9591f938ff4d1440aae87c9fd1c2259b366e3bf1
Fei <unshi@outlook.com>
2020-01-23 18:24:59 -0500

    update jan 23

---
+++ musicstore/musicstore/Controllers/StoreController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using musicstore.Models;
+
+namespace musicstore.Controllers
+{
+    public class StoreController : Controller
+    {
+        public IActionResult AlbumList()
+        {
+            List<Album> albums = new List<Album>();
+            for(int i = 0; i < 10; i++)
+            {
+                albums.Add(new Album { Title = "Album" + i });
+            }
+            ViewBag.Albums = albums;
+            return View(albums);
+        }
+        public IActionResult Sample()
+        {
+            ViewBag.message = "this is viewbag message";
+            return View();
+        }
+
+        public string index()
+        {
+            return "hello index";
+      }
+    }
+}
