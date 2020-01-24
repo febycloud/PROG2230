5dcf75a8d38adf709b15226585b81a52a37d05bc
Fei <unshi@outlook.com>
2020-01-23 23:30:05 -0500

    update

--- musicstore/musicstore/Models/MvcMusicStoreContext.cs
+++ musicstore/musicstore/Models/MvcMusicStoreContext.cs
@@ -29,7 +29,7 @@ namespace musicstore.Models
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                optionsBuilder.UseSqlServer("Server=DESKTOP-O00CET9\\SQLEXPRESS;Database=MvcMusicStore;Trusted_Connection=True;");
+                optionsBuilder.UseSqlServer("ServerFEIWIN10\SQLEXPRESS;Database=MvcMusicStore;Trusted_Connection=True;");
             }
         }
 
